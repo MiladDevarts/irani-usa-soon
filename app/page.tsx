409525e8b235992ebc6ddc69d1652f4d23820ecb
Milad Mohamadi <MiladDevarts@gmail.com>
2023-10-14 01:43:18 +0400

    fix: re-name bg

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main
       className="w-full h-screen flex flex-col items-center justify-center bg-cover bg-no-repeat overflow-hidden"
-      style={{ backgroundImage: `url('/images/bg.jpg')` }}
+      style={{ backgroundImage: `url('/images/soon.jpg')` }}
     >
       <h1 className="text-center text-white font-outfit text-4xl font-semibold">
         On The Way!
