bcba5181627c2c0dcf0a9b86ef60afa4099830f5
Milad Mohamadi <MiladDevarts@gmail.com>
2023-10-14 01:42:34 +0400

    fix: compress image

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main
       className="w-full h-screen flex flex-col items-center justify-center bg-cover bg-no-repeat overflow-hidden"
-      style={{ backgroundImage: `url('/images/bg.png')` }}
+      style={{ backgroundImage: `url('/images/bg.jpg')` }}
     >
       <h1 className="text-center text-white font-outfit text-4xl font-semibold">
         On The Way!
